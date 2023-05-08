76e37128f9e9a8e75da5ab1545598256091b980f
huichun17 <monday20230220@gmail.com>
2023-05-08 21:20:57 +0800

    Vue_路由 & 父子

---
+++ src/components/Child.vue
@@ -0,0 +1,36 @@
+<script >
+
+export default ({
+
+    components: {
+
+    }, props: ["childTitle"],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+})
+</script>
+
+<template>
+    <div class="card">
+        <h2>{{ childTitle }}</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 120px;
+    height: 100px;
+    padding: 2rem;
+    margin: 1rem;
+    border: 5px solid #a73737;
+    ;
+}
+</style>
+
